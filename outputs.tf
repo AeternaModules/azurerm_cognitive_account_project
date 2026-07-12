c5dbfc114acaeadff17cd94c96853fa7c36b71c8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "cognitive_account_projects" {
-  description = "All cognitive_account_project resources"
-  value       = azurerm_cognitive_account_project.cognitive_account_projects
-}
 output "cognitive_account_projects_cognitive_account_id" {
-  description = "List of cognitive_account_id values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.cognitive_account_id]
+  description = "Map of cognitive_account_id values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.cognitive_account_id }
 }
 output "cognitive_account_projects_default" {
-  description = "List of default values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.default]
+  description = "Map of default values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.default }
 }
 output "cognitive_account_projects_description" {
-  description = "List of description values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.description]
+  description = "Map of description values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.description }
 }
 output "cognitive_account_projects_display_name" {
-  description = "List of display_name values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.display_name]
+  description = "Map of display_name values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.display_name }
 }
 output "cognitive_account_projects_endpoints" {
-  description = "List of endpoints values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.endpoints]
+  description = "Map of endpoints values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.endpoints }
 }
 output "cognitive_account_projects_identity" {
-  description = "List of identity values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.identity]
+  description = "Map of identity values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.identity }
 }
 output "cognitive_account_projects_location" {
-  description = "List of location values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.location]
+  description = "Map of location values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.location }
 }
 output "cognitive_account_projects_name" {
-  description = "List of name values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.name]
+  description = "Map of name values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.name }
 }
 output "cognitive_account_projects_tags" {
-  description = "List of tags values across all cognitive_account_projects"
-  value       = [for k, v in azurerm_cognitive_account_project.cognitive_account_projects : v.tags]
+  description = "Map of tags values across all cognitive_account_projects, keyed the same as var.cognitive_account_projects"
+  value       = { for k, v in azurerm_cognitive_account_project.cognitive_account_projects : k => v.tags }
 }
 
